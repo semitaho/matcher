60377aed7f325734f7963e03dcde6467555a5ee8
Toni Aho <toni@tahoo.fi>
2023-08-25 01:07:13 +0300

    geneerinen raattori

--- services/generator.tsx
+++ services/generator.tsx
@@ -1,5 +1,6 @@
 import { MatchDay } from "@/models/match-day.model";
 import { Match } from "@/models/match.model";
+import { match } from "assert";
 
 
 export async function generate(
@@ -8,21 +9,61 @@ export async function generate(
   daysNotToPlay: string[]
 ): Promise<MatchDay[]> {
 
-  const shuffledTeams = shuffleArray(teams);
-  let currentDate = startDate;
-  console.log('current date', shuffledTeams);
+  return createMatrix(teams, startDate);
+}
+
+function createMatrix(teams: string[], startDay: Date): MatchDay[] {
   const matchDays: MatchDay[] = [];
-  while (currentDate <= endDate) {
-    matchDays.push({
-      day: currentDate,
-      matches: createMatchesForDay(currentDate, teams, matchDays)
-    });
-    const result = currentDate.setDate(currentDate.getDate() + 1);
-    currentDate = new Date(result);
+
+
+  for (let days = 0; days < 10; days++) {
+    const matchDay = new Date(startDay);
+    matchDay.setDate(matchDay.getDate() + days);
+    const matchDayObj = findOrCreateMatchDay(matchDays, matchDay);
+    let matchdayTeams = [...teams];
+    let hometeam: string;
+    while (matchdayTeams.length > 1) {
+      do {
+
+        hometeam = random_item(matchdayTeams);
+      } while (hasBeenLately(matchDays, ({ home }) => home === hometeam, days))
+      matchdayTeams = matchdayTeams.filter((matchDayTeam) => matchDayTeam !== hometeam);
+
+      let visitorTeam: string;
+      do {
+
+        visitorTeam = random_item(matchdayTeams);
+      } while (hasFacedHomeLately(matchDays,hometeam, visitorTeam) ||  hasBeenLately(matchDays, ({ visitor }) => visitor === visitorTeam, days))
+      matchdayTeams = matchdayTeams.filter((matchDayTeam) => matchDayTeam !== visitorTeam);
+
+      const match: Match = {
+        home: hometeam,
+        visitor: visitorTeam
+      };
+      matchDayObj.matches.push(match);
+    }
   }
-
   return matchDays;
+}
 
+function hasFacedHomeLately(matchday: MatchDay[], home: string, visitor: string): boolean {
+  return false;
+}
+
+function findOrCreateMatchDay(matchDays: MatchDay[], comparableDay: Date): MatchDay {
+  const matchDay = matchDays.find(({ day }) => comparableDay.getDate() === day.getDate()
+    && comparableDay.getMonth() === day.getMonth() && comparableDay.getFullYear() === day.getFullYear());
+
+  if (matchDay) {
+    return matchDay;
+  }
+  const newMatchDay: MatchDay = {
+    day: comparableDay,
+    matches: []
+
+  };
+  matchDays.push(newMatchDay);
+  return newMatchDay;
 }
 
 function createMatchesForDay(currentDate: Date, teams: string[], matchDays: MatchDay[]): Match[] {
@@ -35,10 +76,10 @@ function createMatchesForDay(currentDate: Date, teams: string[], matchDays: Matc
     if (teamsForGameday.length === 0) break;
     let home = random_item(teamsForGameday);
     teamsForGameday = teamsForGameday
-    .filter(gamedayTeam =>  gamedayTeam !== home);
+      .filter(gamedayTeam => gamedayTeam !== home);
     let visitor = random_item(teamsForGameday);
     teamsForGameday = teamsForGameday
-    .filter(gamedayTeam =>  gamedayTeam !== visitor);
+      .filter(gamedayTeam => gamedayTeam !== visitor);
     matchArr.push({
       home, visitor
     });
@@ -51,11 +92,8 @@ function random_item(items: string[]): string {
   return items[Math.floor(Math.random() * items.length)];
 }
 
-function shuffleArray(teams: string[]): string[] {
-  let shuffled = teams
-    .map(value => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value)
-   
-  return shuffled;
+
+function hasBeenLately(matchDays: MatchDay[], latelyFn: (match: Match) => boolean, gameday: number): boolean {
+  if (gameday === 0) return false;
+  return !!matchDays[gameday - 1].matches.find(latelyFn);
 }
